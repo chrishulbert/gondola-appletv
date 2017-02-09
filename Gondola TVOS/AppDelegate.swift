7a9e20bde68c15d515b950a2a9932a417acea2be
Chris Hulbert <chris.hulbert@gmail.com>
2017-02-09 21:28:11 +1100

    Start on loading

--- Gondola TVOS/AppDelegate.swift
+++ Gondola TVOS/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let loading = LoadingViewController()
+        let nav = UINavigationController(rootViewController: loading)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
         
         MetadataService.request {
             switch $0 {
