f25fba7f644ab3c2ade859e279367756604afc5b
Chris Hulbert <chris.hulbert@gmail.com>
2017-02-17 06:37:54 +1100

    Help

--- Gondola TVOS/ViewControllers/LoadingViewController.swift
+++ Gondola TVOS/ViewControllers/LoadingViewController.swift
@@ -26,6 +26,8 @@ class LoadingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        rootView.help.addTarget(self, action: #selector(tapHelp), for: .primaryActionTriggered)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,9 +40,32 @@ class LoadingViewController: UIViewController {
         
         // Spin if it takes ages.
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.rootView.indicator.alpha = 0
             self.rootView.indicator.startAnimating()
+            UIView.animate(withDuration: 0.3) {
+                self.rootView.indicator.alpha = 1
+            }
         }
         
+        // Display the help button after a while.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.rootView.help.alpha = 0
+            self.rootView.addSubview(self.rootView.help)
+            UIView.animate(withDuration: 0.3, animations: {
+                self.rootView.help.alpha = 1
+            }, completion: { _ in
+                self.setNeedsFocusUpdate()
+            })
+        }
+        
+    }
+    
+    func tapHelp() {
+        let a = UIAlertController(title: "Help",
+                                  message: "Gondola is a media player that connects to a local server.\nIf you'd like to find more information about how to set up your server, please feel free to review the instructions at:\n\nsplinter.com.au/gondola",
+                                  preferredStyle: .alert)
+        a.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+        present(a, animated: true, completion: nil)
     }
     
 }
@@ -51,6 +76,7 @@ class LoadingView: UIView {
     let logoImage = UIImageView(image: #imageLiteral(resourceName: "LaunchLogo"))
     let launchImage = UIImageView(image: UIImage(named: "LaunchImage"))
     let indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.white)
+    let help = UIButton(type: .system)
     
     init() {
         super.init(frame: CGRect.zero)
@@ -62,6 +88,9 @@ class LoadingView: UIView {
         
         addSubview(launchImage)
         addSubview(indicator)
+        
+        help.setTitle("Help", for: .normal)
+        // Don't add it yet, only after a while.
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -77,6 +106,13 @@ class LoadingView: UIView {
         logoImage.frame = bounds
         launchImage.frame = bounds
         indicator.center = CGPoint(x: w/2, y: round(h*3/4))
+        
+        // Center bottom.
+        let helpSize = help.intrinsicContentSize
+        help.frame = CGRect(origin: CGPoint(x: round(w/2 - helpSize.width/2),
+                                            y: round(h - LayoutHelpers.vertMargins - helpSize.height - 8)), // -8 to compensate for focus growth
+            size: helpSize)
+
     }
     
 }
